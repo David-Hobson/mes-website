68341b1d934bfcb6017adba4b953c264af983188
David Hobson <davidhobson96@gmail.com>
2018-08-07 23:03:01 -0400

    Create council model for database usage.

---
+++ models/council.js
@@ -0,0 +1,15 @@
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/mac_eng_society");
+
+var councilSchema = new mongoose.Schema({
+    name: String,
+    position: String,
+    rank: String,
+    program: String,
+    bio: String,
+    thumbnailImage: String,
+    fullImage: String,
+    flavourText: String
+});
+
+module.exports = mongoose.model("Council", councilSchema);
